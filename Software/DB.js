94eccd74b125d81fa00137818774f023137fee81
Teylor Acosta <teylor.acosta@gmail.com>
2026-05-14 15:34:35 -0500

    optimizando

--- Software/DB.js
+++ Software/DB.js
@@ -1,12 +1,27 @@
-require('dotenv').config();
 const mysql = require('mysql2');
 
-// 🔥 POOL CORRECTO
-const db = mysql.createPool({
+const pool = mysql.createPool({
     uri: process.env.MYSQL_URL,
+
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+
+    connectTimeout: 10000,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 });
 
-module.exports = db;
+pool.getConnection((err, connection) => {
+
+    if (err) {
+        console.error('❌ Error conexión MySQL:', err);
+        return;
+    }
+
+    console.log('✅ MySQL conectado');
+
+    connection.release();
+});
+
+module.exports = pool.promise();
