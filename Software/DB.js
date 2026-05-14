3f661025e268a2a9b74a2735539e1dbb8868037e
Teylor Acosta <teylor.acosta@gmail.com>
2026-05-14 15:44:34 -0500

    arreglando

--- Software/DB.js
+++ Software/DB.js
@@ -2,29 +2,40 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
+
     host: 'shinkansen.proxy.rlwy.net',
+
     user: 'root',
+
     password: 'TWPHRfMEWZooysXkytWNJMcFtjmwjDLX',
+
     database: 'railway',
+
     port: 34998,
 
     waitForConnections: true,
+
     connectionLimit: 10,
+
     queueLimit: 0,
 
     connectTimeout: 10000,
+
     enableKeepAlive: true,
+
     keepAliveInitialDelay: 0
 });
 
 pool.getConnection((err, connection) => {
 
     if (err) {
-        console.error('❌ Error conexión MySQL:', err);
+
+        console.error('❌ Error MySQL:', err);
+
         return;
     }
 
-    console.log('✅ MySQL conectado correctamente');
+    console.log('✅ MySQL conectado');
 
     connection.release();
 });
